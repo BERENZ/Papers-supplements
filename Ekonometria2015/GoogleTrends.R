259030565f1fa3fb276b730af89f9b01228d67ea
Maciej Beręsewicz <MaciejBeresewicz@Berenz-2.local>
2015-07-06 12:38:42 +0200

    update

--- Ekonometria2015/GoogleTrends.R
+++ Ekonometria2015/GoogleTrends.R
@@ -1,10 +1,10 @@
 ### new plot
-library(dplyr)
 library(ggplot2)
+library(googletrend)
+library(dplyr)
 library(tidyr)
-library(scales)
-library(readxl)
 library(stringi)
+library(readxl)
 
 ### read short-term statistics
 
@@ -32,29 +32,34 @@ selected_stats <- selected_stats %>%
 
 ### Google data
 
-google_trends <- read.csv(file = 'Ekonometria2015/google_trends.csv',
-                          header= T,
-                          skip = 4,
-                          nrows = 599)
+google_trends <-  googletrend::gettrend(keyword = 'praca',
+                                        geo = 'PL',
+                                        category = '0-958-60',
+                                        use.monthly = F,
+                                        plot = F)
 
 google_trends <- google_trends %>%
-  mutate(date = stri_extract_last_regex(`Tydzień`,'\\d{4}-\\d{2}-\\d{2}'),
-         date = stri_datetime_parse(date,format='%Y-%M-%d')) %>%
-  select(-`Tydzień`) %>%
-  mutate(stat = 'google') %>%
-  rename(value = praca)
+  na.omit() %>%
+  mutate(stat = 'google',
+         week = stri_datetime_parse(week,'%Y-%M-%d')) %>%
+  rename(value = index,
+         date = week)
 
 ### files to plot
 
 to_plot <- bind_rows(selected_stats,google_trends) %>%
   group_by(stat) %>%
-  mutate(value_normalized = (value - min(value))/(max(value)-min(value))) %>%
+  mutate(value_normalized = (value - min(value)) / (max(value) - min(value))) %>%
   ungroup() %>%
-  mutate(stat = factor(x = stat,
-                       levels = c('bezrobotni_indeks','przecietne_zatrudnienie','google'),
-                       labels = c('Bezrobotni zarejestrowani (stan w końcu okresu)',
-                                  'Przeciętne zatrudnienie w sektorze przedsiębiorstw',
-                                  'Google Trends - hasło praca')))
+  mutate(stat = factor(
+    x = stat,
+    levels = c('bezrobotni_indeks','przecietne_zatrudnienie','google'),
+    labels = c(
+      'Bezrobotni zarejestrowani (stan w końcu okresu, okres poprzedni = 100)',
+      'Przeciętne zatrudnienie w sektorze przedsiębiorstw (okres poprzedni = 100)',
+      'Google Trends - hasło praca (dane tygodniowe, stan na koniec tygodnia)'
+    )
+  ))
 
 vline_data <- data_frame(vline = stri_datetime_create(seq(2003,2015,1),12,31))
 
@@ -63,11 +68,13 @@ ggplot(data = to_plot,
            y = value_normalized,
            group = 1)) +
   geom_line() +
-  facet_wrap(~stat,ncol=1) +
+  facet_wrap( ~ stat,ncol = 1) +
   theme_bw() +
   ylab('Wartość indeksu (dane znormalizowane)') +
   xlab('Okres') +
   geom_vline(data = vline_data,
              aes(xintercept = as.numeric(vline)),
-             linetype = 3)
+             linetype = 3) +
+  theme(axis.title = element_text(size = 15),
+        strip.text = element_text(size = 15))
 
